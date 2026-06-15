d6f1d34a8d8d3bfba62e25093038171596c4e881
0-Abdallah-Awwad-0 <abdallah22awwad11@gmail.com>
2026-06-15 21:36:56 +0300

    Add customer store and inventory detail routes

--- BINNA/binnaCustomer/urls.py
+++ BINNA/binnaCustomer/urls.py
@@ -7,11 +7,16 @@ app_name = 'binnaCustomer'
 
 urlpatterns = [
     path('customer_dashboard/', views.customer_dashboard, name='customer_dashboard'),
+
     path('stores_producs/', views.stores_producs, name='stores_producs'),
+    path('stores_producs/<int:supplier_id>/', views.store_details, name='store_details'),
+    path('inventory/<int:inventory_id>/', views.inventory_details, name='inventory_details'),
+
     path('upload/', views.upload_blueprint, name='upload_blueprint'),
     path('result/<int:pk>/', views.blueprint_detail, name='blueprint_detail'),
     path("api/analyze/<int:pk>/", views.analyze_blueprint_ajax, name="analyze_blueprint_ajax"),
     path("ai_assistans/", views.ai_assistans, name="ai_assistans"),
+
     path('customer_settings/', views.customer_settings, name='customer_settings'),
     path('update_customer/', views.update_customer, name='update_customer'),
 ]
